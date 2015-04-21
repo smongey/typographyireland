8104667c51650bac37fd732b7b988729561ee8b6
simonsweeney <hello@simonsweeney.me>
2015-04-21 23:17:18 +0100

    Added more letters

--- call-for-papers/js/main.js
+++ call-for-papers/js/main.js
@@ -3,7 +3,7 @@
 var noise = window.noise;
 noise.seed(Math.random());
 
-var svg = ['svgA', 'svgB', 'svgC', 'svgD', 'svgG', 'svgM', 'svgN', 'svgW', 'svgV', 'svgX'];
+var svg = ['svgA', 'svgB', 'svgC', 'svgD', 'svgG', 'svgH', 'svgI', 'svgJ', 'svgK', 'svgM', 'svgN', 'svgW', 'svgV', 'svgX', 'svgZ'];
 
 // var rand = Math.floor(Math.random() * svg.length);
 // console.log(rand);
