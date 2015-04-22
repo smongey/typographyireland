e0295ac33d846ff100bad1d129fa4cfc23ec0372
simonsweeney <hello@simonsweeney.me>
2015-04-22 15:07:20 +0100

    uncommented the damn letters

--- call-for-papers/js/main.js
+++ call-for-papers/js/main.js
@@ -3,7 +3,7 @@
 var noise = window.noise;
 noise.seed(Math.random());
 
-var svg = [/*'svgA', 'svgB', 'svgC', 'svgD', 'svgE', 'svgG', 'svgH', 'svgI', 'svgJ', 'svgK', 'svgM', 'svgN', 'svgO', 'svgP', 'svgQ', 'svgS', 'svgW', 'svgV', 'svgX', 'svgZ'*/ 'svgR'];
+var svg = ['svgA', 'svgB', 'svgC', 'svgD', 'svgE', 'svgG', 'svgH', 'svgI', 'svgJ', 'svgK', 'svgM', 'svgN', 'svgO', 'svgP', 'svgQ', 'svgR', 'svgS', 'svgW', 'svgV', 'svgX', 'svgZ'];
 
 // var rand = Math.floor(Math.random() * svg.length);
 // console.log(rand);
